17074514afcc9acf2f37b869169717902e77b25f
Joseph Williams <josephwms@utexas.edu>
2024-04-29 03:20:04 -0500

    writeup

--- code/chi_energy_agi.R
+++ code/chi_energy_agi.R
@@ -9,18 +9,6 @@ avg_values = data %>% filter(BUILDING_SUBTYPE == "All")
 avg_values$A00100_av = avg_values$A00100_av*1000
 avg_values$A07260_av = avg_values$A07260_av*1000
 
-# Ensure there are no NA values in key columns
-# data = data %>%
-#   filter(!is.na(A00100) & !is.na(A07260) & !is.na(Latitude) & !is.na(Longitude))
-
-# Calculate the average AGI (A00100) and average Residential Energy Tax Credit (A07260) for each latitude and longitude
-# avg_values = data %>%
-#   group_by(Latitude, Longitude) %>%
-#   summarise(Avg_AGI = mean(A00100, na.rm = TRUE),
-#             Avg_Tax_Credit = mean(A07260, na.rm = TRUE)) %>%
-#   filter(Avg_AGI > 0, Avg_Tax_Credit > 0) %>%
-#   ungroup()
-
 # Apply jitter to the aggregated latitude and longitude for visualization clarity
 set.seed(123)
 avg_values$Jittered_Latitude = jitter(avg_values$Latitude, amount = 0.001)
@@ -37,7 +25,7 @@ chicago_boundaries = readLines("../working_data/Boundaries - Neighborhoods.geojs
 chicago_boundaries = paste(chicago_boundaries, collapse = "\n")
 
 # Create the map with AGI data and add the Chicago city boundaries
-map = leaflet(avg_values) %>%
+vis01 = leaflet(avg_values) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(
     ~Jittered_Longitude, ~Jittered_Latitude,
@@ -46,7 +34,7 @@ map = leaflet(avg_values) %>%
     fillOpacity = 0.8,
     stroke = FALSE,
     popup = ~paste("Average AGI: $", comma(A00100_av), "<br>",
-                   "Average Residential Energy Tax Credit: $", comma(A07260_av))
+                   "Average Energy Credit: $", comma(A07260_av))
   ) %>%
   addGeoJSON(geojson = chicago_boundaries, 
              weight = 1, 
@@ -55,7 +43,7 @@ map = leaflet(avg_values) %>%
              fillOpacity = 0
   ) %>%
   addLegend("topright", pal = tax_credit_pal, values = ~A07260_av,
-            title = "Average Residential Energy Tax Credit",
+            title = "Average Energy Credit",
             opacity = 1,
             ) %>%
   setView(lng = -87.6298, lat = 41.8781, zoom = 10)
@@ -63,12 +51,16 @@ map = leaflet(avg_values) %>%
 
 
 
+#Save vis01, etc. to figures folder under these names
+vis01
+
+
 
 # Scale the radius of the circles based on THERMS
 avg_values$Radius = sqrt(avg_values$THERMS.PER.SQFT)*6
 
 # Create the map with Therms data and add the Chicago city boundaries
-map = leaflet(avg_values) %>%
+vis02 = leaflet(avg_values) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(
     ~Jittered_Longitude, ~Jittered_Latitude,
@@ -76,8 +68,8 @@ map = leaflet(avg_values) %>%
     color = ~tax_credit_pal(A07260_av),
     fillOpacity = 0.8,
     stroke = FALSE,
-    popup = ~paste("Therms/TotalSQFT: T/ft^2", comma(A00100_av), "<br>",
-                   "Average Residential Energy Tax Credit: $", comma(A07260_av))
+    popup = ~paste("Therms/TotalSQFT: T/ft^2", comma(THERMS.PER.SQFT), "<br>",
+                   "Average Energy Credit: $", comma(A07260_av))
   ) %>%
   addGeoJSON(geojson = chicago_boundaries, 
              weight = 1, 
@@ -86,7 +78,7 @@ map = leaflet(avg_values) %>%
              fillOpacity = 0
   ) %>%
   addLegend("topright", pal = tax_credit_pal, values = ~A07260_av,
-            title = "Average Residential Energy Tax Credit",
+            title = "Average Energy Credit",
             opacity = 1,
   ) %>%
   setView(lng = -87.6298, lat = 41.8781, zoom = 10)
@@ -94,16 +86,16 @@ map = leaflet(avg_values) %>%
 
 
 # Print the map
-map
+vis02
 
 
 ##CircleSize _> KWH
 # Scale the radius of the circles based on KWH
-avg_values$Radius = sqrt(avg_values$KWH.PER.SQFT)*3
+avg_values$Radius = sqrt(avg_values$KWH.PER.SQFT)*2.5
 
 
 # Create the map with Therms data and add the Chicago city boundaries
-map = leaflet(avg_values) %>%
+vis03 = leaflet(avg_values) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(
     ~Jittered_Longitude, ~Jittered_Latitude,
@@ -111,8 +103,8 @@ map = leaflet(avg_values) %>%
     color = ~tax_credit_pal(A07260_av),
     fillOpacity = 0.8,
     stroke = FALSE,
-    popup = ~paste("KWH/TotalSQFT: KWH/ft^2", comma(A00100_av), "<br>",
-                   "Average Residential Energy Tax Credit: $", comma(A07260_av))
+    popup = ~paste("KWH/TotalSQFT: KWH/ft^2", comma(KWH.PER.SQFT), "<br>",
+                   "Average Energy Credit: $", comma(A07260_av))
   ) %>%
   addGeoJSON(geojson = chicago_boundaries, 
              weight = 1, 
@@ -121,7 +113,7 @@ map = leaflet(avg_values) %>%
              fillOpacity = 0
   ) %>%
   addLegend("topright", pal = tax_credit_pal, values = ~A07260_av,
-            title = "Average Residential Energy Tax Credit",
+            title = "Average Energy Credit",
             opacity = 1,
   ) %>%
   setView(lng = -87.6298, lat = 41.8781, zoom = 10)
@@ -129,6 +121,6 @@ map = leaflet(avg_values) %>%
 
 
 # Print the map
-map
+vis03
 
 
